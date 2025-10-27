c224ef7acfcf1599ce7a25822e73bb535ad59c7d
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 17:15:39 -0400

    Add local variables for EKS setup
    Define local variables for EKS cluster configuration.

---
+++ terraform_code/eks_code/local.tf
@@ -0,0 +1,14 @@
+locals {
+  cluster_name = "kingsley"
+  region       = "us-east-1"
+  vpc_cidr     = "10.0.0.0/16"
+  azs          = ["us-east-1a", "us-east-1b"]
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  tags = {
+    Environment = "dev"
+    Project     = local.cluster_name
+  }
+}
