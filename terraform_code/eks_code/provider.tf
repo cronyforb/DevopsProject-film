98914bb38cd4c418d65a9287e5ce9b4e163adfef
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 17:23:07 -0400

    Update provider.tf

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 6.20.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.2"
     }
   }
-}
 
-provider "aws" {
-  region = local.region
+  required_version = ">= 1.5.0"
 }
