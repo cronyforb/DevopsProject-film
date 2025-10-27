4992d109ac8442b83eabd6b5333e93c35eaa7890
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 16:49:38 -0400

    Change cluster_name to local variable
    Updated cluster_name to use local variable.

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -2,14 +2,12 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  # Cluster configuration
-  cluster_name    = "amazon-prime-cluster"
+  cluster_name    = local.name
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # Managed Node Group
   eks_managed_node_groups = {
     panda-node = {
       desired_capacity = 2
