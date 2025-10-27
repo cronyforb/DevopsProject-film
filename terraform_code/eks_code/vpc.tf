d6107f3c417514decd92e2b8da8e5440edd71189
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 16:38:37 -0400

    Update VPC module version and cluster name

--- terraform_code/eks_code/vpc.tf
+++ terraform_code/eks_code/vpc.tf
@@ -1,25 +1,23 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
+  version = "5.16.0"
 
-  name = "amazon-prime-cluster"
+  name = local.cluster_name
   cidr = local.vpc_cidr
 
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/role/elb" = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/internal-elb" = "1"
   }
 
   tags = local.tags
 }
-
