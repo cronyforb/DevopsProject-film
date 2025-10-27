3c0203a6cdc3747572e2bb8a1a5906f93a62e4e7
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 17:17:20 -0400

    Upgrade Terraform version and refactor provider settings
    Updated Terraform required version and provider configuration.

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,29 +1,14 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = local.region
 }
-
-locals {
-  region          = "us-east-1"
-  name            = "local.name"
-  vpc_cidr        = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
-  tags = {
-    Name = "kingsley"
-  }
-}
-
-
