183468c3ab273e69ebca9a90acf80e7540dde85c
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 17:16:16 -0400

    Update VPC module settings in vpc.tf
    Refactor VPC module configuration and add DNS support.

--- terraform_code/eks_code/vpc.tf
+++ terraform_code/eks_code/vpc.tf
@@ -1,36 +1,18 @@
-# locals.tf
-locals {
-  name      = "kingsley"
-  vpc_cidr  = "10.0.0.0/16"
-  azs       = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
-
-  tags = {
-    Name = local.name
-  }
-}
-
-# provider.tf
-provider "aws" {
-  region = "us-east-1"
-}
-
-# vpc.tf
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
-  name = local.name
+  name = local.cluster_name
   cidr = local.vpc_cidr
 
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
 
-  enable_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -38,35 +20,6 @@ module "vpc" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = local.tags
-}
-
-# eks.tf
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
-
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    kingsley-node = {
-      min_size     = 2
-      max_size     = 4
-      desired_size = 2
-
-      instance_types = ["t2.medium"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "KINGSLEY_Node"
-      }
-    }
   }
 
   tags = local.tags
