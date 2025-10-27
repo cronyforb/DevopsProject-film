681adb50bb760582613d647d4b953adff78d14f1
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 16:37:30 -0400

    Refactor provider.tf for Terraform configuration

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,17 +1,26 @@
-locals {
-  region        = "us-east-1"
-  name          = "amazon-prime-cluster"
-  vpc_cidr      = "10.0.0.0/16"
-  azs           = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+lterraform {
+  required_version = ">= 1.5.0"
 
-  tags = {
-    Example = "amazon-prime-cluster"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
 
 provider "aws" {
-  region = local.region
+  region = "us-east-1"
+}
+
+locals {
+  cluster_name = "amazon-prime-cluster"
+  vpc_cidr     = "10.0.0.0/16"
+  azs          = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  tags = {
+    Name = local.cluster_name
+  }
 }
