35f56d427ac134b704fe552f085231eb2e57df6c
cronyforb <113175794+cronyforb@users.noreply.github.com>
2025-10-27 17:16:48 -0400

    Update eks.tf

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -1,25 +1,25 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "21.6.1"
 
-  cluster_name    = "kingsleye"  # Must follow AWS EKS naming rules
+  cluster_name    = local.cluster_name
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access = true
+
   eks_managed_node_groups = {
-    panda-node = {
-      desired_capacity = 2
-      min_size         = 1
-      max_size         = 3
-      instance_types   = ["t2.medium"]
+    kingsley_nodes = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
     }
   }
 
-  # Tags for the cluster
-  tags = {
-    Name = "kingsley"
-  }
+  tags = local.tags
 }
-
